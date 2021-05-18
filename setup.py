569f7e660d41627cfe5e6d65999108eba5b66d03
asleep-cult <asleep.cult@gmail.com>
2021-05-18 08:35:53 -0400

    License, setup.py, __init__.py, emojis.py => data.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    author='asleep-cult',
+    name='snakecord-emojis',
+    description='All Discord emojis for snakecord',
+    packages=['snakecord.emojis'],
+    version='0.0.1'
+)
