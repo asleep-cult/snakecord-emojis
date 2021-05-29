37651b32e465dd4d3a34d8d0188721ebaf92e901
asleep-cult <asleep.cult@gmail.com>
2021-05-29 11:58:03 -0400

    anand forced me to

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 
 setuptools.setup(
     author='asleep-cult',
-    name='snakecord-emojis',
-    description='All Discord emojis for snakecord',
-    packages=['snakecord.emojis'],
+    name='snekcord-emojis',
+    description='All Discord emojis for snekcord',
+    packages=['snekcord.emojis'],
     version='0.0.2',
-    url='https://github.com/asleep-cult/snakecord-emojis',
+    url='https://github.com/asleep-cult/snekcord-emojis',
 )
